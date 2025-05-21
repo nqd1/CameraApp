02f838e75b9e7c457f3044347e13831422f6e99b
Nguyen Quy Duc <nqd001205@gmail.com>
2025-05-21 16:44:51 +0700

    Camera Request

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,45 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
-
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final ActivityResultLauncher<String> requestLauncherPermissions =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    accessCamera();
+                } else {
+                    Toast.makeText(this, "Camera Permission required.", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestCameraPermissions();
+        } else {
+            accessCamera();
+        }
+    }
+
+    private void requestCameraPermissions() {
+        requestLauncherPermissions.launch(Manifest.permission.CAMERA);
+    }
+
+    private void accessCamera() {
+        // Thêm logic truy cập camera ở đây
+        Toast.makeText(this, "Camera Accessed", Toast.LENGTH_SHORT).show();
     }
 }
